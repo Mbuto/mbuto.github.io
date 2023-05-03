f544e32a94a3321c14f6f9f8dff9a709e285d99b
mbuto11 <mbuto11@gmail.com>
2023-05-03 17:01:44 +0200

    chmod

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 // outputs the username that owns the running php/httpd process
 // (on a system with the "whoami" executable in the path)
-//$output=null;
-//$retval=null;
+$output=null;
+$retval=null;
 //exec('./tophost-lu', $output, $retval);
-//echo "Returned with status $retval and output:\n";
-//print_r($output);
-$output = shell_exec('./lu.sh');
-echo "$output";
+echo "Returned with status $retval and output:\n";
+print_r($output);
+//$output = shell_exec('./lu.sh');
+//echo "$output";
 //exec('./lu.sh', $output, $retval);
 //echo "Returned with status $retval and output:\n";
 //print_r($output);
